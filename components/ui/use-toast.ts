41e074c9d9a8c04db340c6e9adc22627088f032e
the-game-master79 <oddkingz12@gmail.com>
2025-07-14 14:17:28 +0530

    Update use-toast.ts

--- components/ui/use-toast.ts
+++ components/ui/use-toast.ts
@@ -13,14 +13,12 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement;
 };
 
-const actionTypes = {
+const ACTION_TYPES = {
   ADD_TOAST: 'ADD_TOAST',
   UPDATE_TOAST: 'UPDATE_TOAST',
   DISMISS_TOAST: 'DISMISS_TOAST',
   REMOVE_TOAST: 'REMOVE_TOAST',
 } as const;
-
-type ActionType = typeof actionTypes;
 
 let count = 0;
 
@@ -31,19 +29,19 @@ function genId() {
 
 type Action =
   | {
-      type: ActionType['ADD_TOAST'];
+      type: typeof ACTION_TYPES.ADD_TOAST;
       toast: ToasterToast;
     }
   | {
-      type: ActionType['UPDATE_TOAST'];
+      type: typeof ACTION_TYPES.UPDATE_TOAST;
       toast: Partial<ToasterToast>;
     }
   | {
-      type: ActionType['DISMISS_TOAST'];
+      type: typeof ACTION_TYPES.DISMISS_TOAST;
       toastId?: ToasterToast['id'];
     }
   | {
-      type: ActionType['REMOVE_TOAST'];
+      type: typeof ACTION_TYPES.REMOVE_TOAST;
       toastId?: ToasterToast['id'];
     };
 
