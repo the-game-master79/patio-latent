971700d1baca8a3874ecd059b234664fabca0d2a
the-game-master79 <oddkingz12@gmail.com>
2025-07-14 14:55:39 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Footer } from "@/components/footer";
-import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,7 +22,6 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
-        <Toaster />
       </body>
     </html>
   );
